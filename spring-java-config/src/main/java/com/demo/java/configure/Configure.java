cc373eee7b1ad0b7f85d3a36bd581ec10684be2c
danhho <congdanh910@gmail.com>
2015-05-25 08:27:24 +0700

    Spring base on Java configuration

---
+++ spring-java-config/src/main/java/com/demo/java/configure/Configure.java
@@ -0,0 +1,28 @@
+package com.demo.java.configure;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.JstlView;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
+
+//Marks this class as configuration
+@Configuration
+// Specifies which package to scan
+@ComponentScan(basePackages = { "com.demo.java" })
+// Enables Spring's annotations
+@EnableWebMvc
+public class Configure extends WebMvcConfigurerAdapter {
+
+	@Bean
+	public UrlBasedViewResolver setupViewResolver() {
+		UrlBasedViewResolver resolver = new UrlBasedViewResolver();
+		resolver.setPrefix("/WEB-INF/views/");
+		resolver.setSuffix(".jsp");
+		resolver.setViewClass(JstlView.class);
+		return resolver;
+	}
+
+}
