ac121db12e357fb5f51ebe1447a8a467fa5281d8
danhho <congdanh910@gmail.com>
2015-07-27 22:15:08 +0700

    Remove old project

--- mongodb-spring/src/main/java/com/demo/mongodb/utils/Constant.java
+++
@@ -1,6 +0,0 @@
-package com.demo.mongodb.utils;
-
-public class Constant {
-	public static final String MONGGO_COLLECTION_TEST = "test";
-	public static final String MONGGO_COLLECTION_LEAD = "lead";
-}
