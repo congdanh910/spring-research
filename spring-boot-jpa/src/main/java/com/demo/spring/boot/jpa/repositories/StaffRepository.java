59a98ea6711ff600e8da63b554a643d6bed2d7bd
congdanh910 <congdanh910@gmail.com>
2015-11-02 13:35:26 +0700

    Spring-boot-jpa : - Adding the comment for @Transaction between DAO vs Service layer

--- spring-boot-jpa/src/main/java/com/demo/spring/boot/jpa/repositories/StaffRepository.java
+++ spring-boot-jpa/src/main/java/com/demo/spring/boot/jpa/repositories/StaffRepository.java
@@ -5,7 +5,12 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.demo.spring.boot.jpa.model.Staff;
-//@Transactional(readOnly = true) ==> In fact, we can use @Transactional here but we should move it to Service layer 
+/*
+   @Transactional(readOnly = true) ==> In fact, we can use @Transactional here but we should move it to Service layer 
+    http://stackoverflow.com/questions/19716673/spring-data-jpa-transactional
+      The transaction configuration at the repositories will be neglected 
+      then as the outer transaction configuration determines the actual one used.
+ */
 public interface StaffRepository extends CrudRepository<Staff, Long> {
   
   @Modifying
